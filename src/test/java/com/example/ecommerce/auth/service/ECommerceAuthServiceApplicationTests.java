773a5ba29d55a06ca93af311b58d45f263e06df5
donghun0508 <alswn4516@gmail.com>
2024-03-30 16:26:32 +0900

    first commit

---
+++ src/test/java/com/example/ecommerce/auth/service/ECommerceAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
